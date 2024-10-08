dceb987182c9f826f622a06da77c38d27004bdcc
Tomáš Calábek <124521380+siberiacaly@users.noreply.github.com>
2024-10-08 16:04:31 +0200

    Add files via upload

---
+++ dsacv4/src/main/java/cv4/Dsacv4.java
@@ -0,0 +1,8 @@
+package cv4;
+
+public class Dsacv4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ dsacv4/src/main/java/cv4/RTP.java
@@ -0,0 +1,16 @@
+package cv4;
+
+public class RTP {
+    public RTP next;
+    public int value;
+
+	public RTP getNext() {
+		return next;
+	}
+
+	public void setNext(RTP next) {
+		this.next = next;
+	}
+
+
+}
